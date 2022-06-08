f551a860bca1c3b8def6fc39a24d121b59158f12
Aleksey <xdeesiigneerx@gmail.com>
2022-06-08 10:53:05 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ requires = ['requests==2.25.1']
 
 setup(
     name='pyspapi',
-    version='1.0.0a',
-    description='Framework for SP API',
+    version='1.0.2',
+    description='API wrapper for SP servers written in Python',
     long_description=description,
     long_description_content_type='text/markdown',
     author='deesiigneer',
