b4ddd1fd93c4e58775d9cbc8a3586f5129dac9ae
deesiigneer <xdeesiigneerx@gmail.com>
2022-07-18 22:27:10 +0300

    update example

--- examples/spapi/get_user and get_users.py
+++ examples/spapi/get_user and get_users.py
@@ -4,4 +4,8 @@ spapi = pyspapi.SPAPI(card_id='card_id', token='token')
 
 print(spapi.get_user(262632724928397312))
 
+print(spapi.get_user(262632724928397312).username)
+
+print(spapi.get_user(262632724928397312).access)
+
 print(spapi.get_users([262632724928397312, 264329096920563714]))
