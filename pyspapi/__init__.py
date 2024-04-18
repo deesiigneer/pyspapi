14166cf5199eb25d69a7e9750c1914d78e7ed582
deesiigneer <goldenrump@gmail.com>
2024-04-18 22:38:23 +0500

    version update

--- pyspapi/__init__.py
+++ pyspapi/__init__.py
@@ -7,4 +7,4 @@ __author__ = 'deesiigneer'
 __url__ = 'https://github.com/deesiigneer/pyspapi'
 __description__ = 'API wrapper for SP servers written in Python.'
 __license__ = 'MIT'
-__version__ = "3.1.0"
+__version__ = "3.1.1"
