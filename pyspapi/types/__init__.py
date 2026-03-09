2e75de605d7d479587f6630f0675b4211498c086
deesiigneer <xdeesiigneerx@gmail.com>
2026-03-09 12:25:57 +0000

    fix: include UserCards in exported module members

--- pyspapi/types/__init__.py
+++ pyspapi/types/__init__.py
@@ -1,5 +1,5 @@
 from pyspapi.types.me import Account
 from pyspapi.types.payment import Item
-from pyspapi.types.users import User
+from pyspapi.types.users import User, UserCards
 
-__all__ = ["Account", "Item", "User"]
+__all__ = ["Account", "Item", "User", "UserCards"]
